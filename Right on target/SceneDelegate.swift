dc8ec00921dfae8c0fa086a022f6a1d228b9d833
Margarita Novokh <r28611@gmail.com>
2021-12-17 13:46:27 +0100

    bug fixing

--- Right on target/SceneDelegate.swift
+++ Right on target/SceneDelegate.swift
@@ -18,10 +18,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
-        window = UIWindow(frame: UIScreen.main.bounds)
+        window = UIWindow(windowScene: windowScene)
         guard let window = window else { return }
-
-        window.windowScene = windowScene
 
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let viewController = storyboard.instantiateViewController(withIdentifier: "MainViewController") as! MainViewController
